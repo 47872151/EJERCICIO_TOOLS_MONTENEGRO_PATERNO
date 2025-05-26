d6d18e2bcd20acb5343ab9f018fb876037f8637a
47872151 <47872151@est.ort.edu.ar>
2025-05-26 16:15:58 -0300

    subir

---
+++ src/lib/estudiantes.js
@@ -0,0 +1,72 @@
+// Gestión de estudiantes
+import { readFileSync, writeFileSync } from 'fs';
+
+const DATA_FILE = './data/alumnos.json';
+
+class Estudiantes {
+  constructor() {
+    this.estudiantes = [];
+  }
+  
+  cargarEstudiantesDesdeJson() {
+    try {
+        const data = JSON.parse(readFileSync(DATA_FILE, 'utf-8'));
+        this.estudiantes = data.alumnos || [];
+    } catch (e) {
+        console.error("Error al leer el archivo de datos:", e);
+    }
+  }
+
+  guardarEstudiantes() {
+    try {
+      writeFileSync(DATA_FILE, JSON.stringify({ alumnos: this.estudiantes }, null, 2));
+      this.cargarEstudiantesDesdeJson();
+    } catch (e) {
+      console.error("Error al guardar los estudiantes:", e);
+      throw new Error("No se pudo guardar la lista de estudiantes.");
+    }
+  }
+
+  // TODO: Implementar método para agregar estudiante
+  agregarEstudiante(nombre, apellido, curso) {
+    // Tu código aquí
+    try {
+      const estudiante = new Estudiantes(nombre, apellido, curso);
+      let msj = "";
+      if(!this.estudiantes.includes(estudiante))
+      {
+        writeFileSync(DATA_FILE, JSON.stringify((this.estudiantes).push(nombre, apellido, curso))); 
+        this.cargarEstudiantesDesdeJson();
+        msj = "El nuevo estudiante ha sido agregado"
+      }
+      else
+      {
+         msj = "Ya está en la lista"
+      }
+      
+      return msj;
+    } catch (e) {
+      console.error("Error al guardar los estudiantes:", e);
+      throw new Error("No se pudo guardar la lista de estudiantes.");
+    }
+  }
+
+  // TODO: Implementar método para buscar estudiante por nombre
+  buscarEstudiantePorNombre(nombre) {
+    console.log(this.estudiantes.filter(estudiante => estudiante.nombre === nombre))
+    return this.estudiantes.filter(estudiante => estudiante.nombre === nombre);
+  }
+
+  // TODO: Implementar método para buscar estudiante por apellido
+  buscarEstudiantePorApellido(apellido) {
+    return this.estudiantes.filter(estudiante => estudiante.apellido === apellido);
+  }
+
+  // TODO: Implementar método para listar estudiantes
+  listarEstudiantes() {
+    console.log(this.estudiantes)
+    return this.estudiantes;
+  }
+}
+
+export { Estudiantes }
